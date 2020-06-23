33a6f535c7ec44a15668863fc770c90d718aa428
Insidium <ryanchallen@outlook.com>
2020-06-23 15:22:36 +1000

    Created navbar component with link to dashboard and home, set up app.js with navbar, router and container div

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import AppNavBar from './components/layout/AppNavBar.js';
 import './App.css';
 
 function App() {
 	return (
-		<div className='App'>
-			<h1>Hello there!</h1>
-		</div>
+		<Router>
+			<div className='App'>
+				<AppNavBar />
+				<div className='container'>
+					<h1>Hello there!</h1>
+				</div>
+			</div>
+		</Router>
 	);
 }
 
