bc40926d4b735db893e392e74812334879a32545
Insidium <ryanchallen@outlook.com>
2020-06-24 11:31:26 +1000

    Completed clients component by adding access to total funds outstanding on dashboard, connecting to firestore clients

--- src/components/clients/Clients.js
+++ src/components/clients/Clients.js
@@ -7,8 +7,26 @@ import { firestoreConnect } from 'react-redux-firebase';
 import Spinner from '../layout/Spinner';
 
 class Clients extends Component {
+	state = {
+		totalOwed: null
+	};
+
+	static getDerivedStateFromProps(props, state) {
+		const { clients } = props;
+		if (clients) {
+			// Add balances
+			const total = clients.reduce((total, client) => {
+				return total + parseFloat(client.balance.toString());
+			}, 0);
+			return { totalOwed: total };
+		} else {
+			return null;
+		}
+	}
+
 	render() {
 		const { clients } = this.props;
+		const { totalOwed } = this.state;
 
 		if (clients) {
 			return (
@@ -16,10 +34,18 @@ class Clients extends Component {
 					<div className='row'>
 						<div className='col-md-6'>
 							<h2>
-								{''} <i className='fas fa-users'></i> Clients{''}
+								{' '}
+								<i className='fas fa-users'></i> Clients{' '}
 							</h2>
 						</div>
-						<div className='col-md-6'></div>
+						<div className='col-md-6'>
+							<h5 className='text-right text-secondary'>
+								Total Outstanding:{' '}
+								<span className='text-primary'>
+									${parseFloat(totalOwed).toFixed(2)}
+								</span>
+							</h5>
+						</div>
 						<table className='table table-striped'>
 							<thead className='thead-inverse'>
 								<tr>
