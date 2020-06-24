23eeea55fc453ee6eaed9104db4d69a2ac792515
Insidium <ryanchallen@outlook.com>
2020-06-24 10:20:18 +1000

    Commented out firestore init to avoid issues, added Provider with store to app, reverted to redux-firebase/firestore prev versions as compose was breaking

--- src/App.js
+++ src/App.js
@@ -1,21 +1,25 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from './store';
 import AppNavBar from './components/layout/AppNavBar';
 import Dashboard from './components/layout/Dashboard';
 import './App.css';
 
 function App() {
 	return (
-		<Router>
-			<div className='App'>
-				<AppNavBar />
-				<div className='container'>
-					<Switch>
-						<Route exact path='/' component={Dashboard} />
-					</Switch>
+		<Provider store={store}>
+			<Router>
+				<div className='App'>
+					<AppNavBar />
+					<div className='container'>
+						<Switch>
+							<Route exact path='/' component={Dashboard} />
+						</Switch>
+					</div>
 				</div>
-			</div>
-		</Router>
+			</Router>
+		</Provider>
 	);
 }
 
