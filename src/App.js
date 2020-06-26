4548dc84e74582eecf967cdd4deffd989c6e0b6b
Insidium <ryanchallen@outlook.com>
2020-06-27 09:47:13 +1000

    Created login component, changed auth rules for firebase to req auth, imported and routed in app.js

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Dashboard from './components/layout/Dashboard';
 import AddClient from './components/clients/AddClient';
 import EditClient from './components/clients/EditClient';
 import ClientDetails from './components/clients/ClientDetails';
+import Login from './components/auth/Login';
 import './App.css';
 
 function App() {
@@ -21,6 +22,7 @@ function App() {
 							<Route exact path='/client/add' component={AddClient} />
 							<Route exact path='/client/edit/:id' component={EditClient} />
 							<Route exact path='/client/:id' component={ClientDetails} />
+							<Route exact path='/login' component={Login} />
 						</Switch>
 					</div>
 				</div>
