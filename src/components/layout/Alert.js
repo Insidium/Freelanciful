11992f562d63fbffb4c900047afe2d34b8caa918
Insidium <ryanchallen@outlook.com>
2020-06-29 11:52:29 +1000

    Created notify actions to define message and type of notification, added classnames and specified them for the messages shown to user on correct/incorrect login

--- src/components/layout/Alert.js
+++ src/components/layout/Alert.js
@@ -3,8 +3,22 @@ import classnames from 'classnames';
 import PropTypes from 'prop-types';
 
 const Alert = (props) => {
-	const { message, messageType } = this.props;
-	return <div></div>;
+	const { message, messageType } = props;
+	return (
+		<div
+			className={classnames('alert', {
+				'alert-success': messageType === 'success',
+				'alert-danger': messageType === 'error'
+			})}
+		>
+			{message}
+		</div>
+	);
+};
+
+Alert.propTypes = {
+	message: PropTypes.string.isRequired,
+	messageType: PropTypes.string.isRequired
 };
 
 export default Alert;
